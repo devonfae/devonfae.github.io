f2c3737e82e27f26ce0500fd5e8bc00f30f6b039
Devon Fae <42707378+devonfae@users.noreply.github.com>
2024-12-28 19:59:13 -0500

    Add files via upload

---
+++ lit-core/more-expressions.js
@@ -0,0 +1,27 @@
+import {LitElement, html} from 'lit-core.min.js';
+
+export class MoreExpressions extends LitElement {
+  static properties = {
+    checked: {},
+  };
+
+  constructor() {
+    super();
+    this.checked = false;
+  }
+
+  render() {
+    return html`
+      <div>
+         <!-- TODO: Add expression to input. -->
+         <input type="text" value="Hello there.">
+      </div>
+      <label><input type="checkbox" @change=${this.setChecked}> Enable editing</label>
+    `;
+  }
+
+  setChecked(event) {
+    this.checked = event.target.checked;
+  }
+}
+customElements.define('more-expressions', MoreExpressions);
